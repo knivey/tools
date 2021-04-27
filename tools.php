be8393457fecf5e63de9ecd311d1e40485f53f5a
knivey <knivey@botops.net>
2021-04-26 20:22:34 -0700

    add stuff

--- tools.php
+++ tools.php
@@ -45,3 +45,213 @@ function dirtree(string $dir, string $extension = "txt"): array {
     return $tree;
 }
 
+/**
+ * Takes a string of byte data and converts it to an int
+ * @param string $s
+ * @return number
+ */
+function str2int($s) {
+    $hex = hexdump($s);
+    return (int)hexdec($hex);
+}
+
+/**
+ * Turn Bytes size into human readable format
+ * @param int $size
+ * @return string
+ */
+function convert($size) {
+    $unit=array('b','kb','mb','gb','tb','pb');
+    return @round($size/pow(1024,($i=floor(log($size,1024)))),2).$unit[$i];
+}
+
+/**
+ * Reverse the order of data string
+ * @param string $s
+ * @return string
+ */
+function revbo($s) {
+    $s = str_split($s);
+    $s = array_reverse($s);
+    $s = implode('', $s);
+    return $s;
+}
+
+/**
+ * Provides a printable hex string from a data string
+ * @param string $s
+ * @return string
+ */
+function hexdump($s) {
+    $s = str_split($s);
+    $out = '';
+    foreach($s as $c) {
+        $hex = dechex(ord($c));
+        if(strlen($hex) == 1) {
+            $hex = '0' . $hex;
+        }
+        $hex = strtoupper($hex);
+        $out .= "$hex ";
+    }
+    return trim($out);
+}
+
+/**
+ * Searches $ar until it finds case insensitive match for $key
+ * and returns it, or NULL on fail.
+ * @param mixed $key
+ * @param Array $ar
+ * @return mixed
+ */
+function get_akey_nc($key, $ar) {
+    if(is_array($ar)) {
+        $keys = array_keys($ar);
+        foreach($keys as &$k) {
+            if(strtolower($key) == strtolower($k))
+                return $k;
+        }
+    }
+    return NULL;
+}
+
+/**
+ * Pad all elements of an array with spaces so they contain the same number of characters
+ * Also will add one space to the existing max length
+ * @param array $array
+ * @return array
+ */
+function array_padding($array) {
+    $pad = 0;
+    for ($i = 0; $i < count($array); $i++) {
+        if (strlen($array[$i]) - substr_count($array[$i], "\2") > $pad) {
+            $pad = strlen($array[$i]) - substr_count($array[$i], "\2");
+        }
+    }
+    for ($i = 0; $i < count($array); $i++) {
+        if($pad - strlen($array[$i]) + substr_count($array[$i], "\2") + 1 > 0) {
+            $array[$i] = $array[$i] . str_repeat(' ',$pad - strlen($array[$i]) + substr_count($array[$i], "\2") + 1);
+        }
+    }
+    return $array;
+}
+
+/**
+ * Takes a 2D Array and pads it to make a nicely printable table.
+ * The first dimension of the array is the rows, the second columns.
+ * @param array $array
+ * @return array
+ */
+function multi_array_padding($array) {
+    //First dimension is rows second is cols
+    $c = 0;$r = 0;
+    $col = Array();
+    $cols = count($array[0]);
+    $rows = count($array);
+    for($c = 0; $c < $cols; $c++) { // go through each col
+        for($r = 0; $r < $rows; $r++) { // go through each row
+            $col[] = $array[$r][$c];
+        }
+        $col = array_padding($col);
+        for($r = 0; $r < $rows; $r++) { // go through each row again and reset it
+            $array[$r][$c] = $col[$r];
+        }
+        $col = Array();
+    }
+    return $array;
+}
+
+/**
+ * Get the time including microseconds as a float
+ * @return float
+ * @codeCoverageIgnore
+ */
+function microtime_float()
+{
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
+}
+
+/**
+ * This will make an argv array while treating "quoatable(\") arguments"
+ * as one argument in the array. If there is an error it will return
+ * a number indicating the posistion of the error. (ugly)
+ * @param string $string
+ * @return array|int
+ */
+function makeArgs($string) {
+    /* 7/3/09 - Finished
+     * take $string  and make args split by
+     * spaces but including " support and \ support
+     * then return as an array or a pos of error
+     */
+    $s = str_split($string);
+    $skip = false;
+    $pos = -1;
+    $args = Array();
+    $inQuote = false;
+    $lastChar = ' ';
+
+    $curArg = 0;
+    $Bskip = false;
+
+    foreach ($s as $c) {
+        $pos++;
+        if ($Bskip) {
+            $Bskip = false; // skip adding char too
+            $lastChar = $c;
+            continue;
+        }
+        if ($c == "\\") {
+            $skip = true; //skip over next char
+            $lastChar = $c;
+            continue;
+        }
+        if ($skip) {
+            $skip = false; // only skip one char
+            $lastChar = $c;
+            if(!array_key_exists($curArg, $args)) {
+                $args[$curArg] = '';
+            }
+            $args[$curArg] .= $c;
+            continue;
+        }
+
+        if ($c == '"') {
+            if ($inQuote == false) {
+                if ($lastChar != ' ') { //Quote should only come at begin of arg
+                    return $pos+1; // Error at pos
+                }
+                $inQuote = true;
+                $lastChar = $c;
+                continue;
+            } else {
+                if (isset($string[$pos + 1]) && $string[$pos + 1] != ' ') {
+                    //only end or space should follow end quote
+                    return $pos + 1;
+                }
+                if ($lastChar == '"' && !isset($args[$curArg])) {
+                    $args[$curArg] = '';
+                }
+                $Bskip = true; //skip next space
+                $inQuote = false;
+                $curArg++;
+                $lastChar = $c;
+                continue;
+            }
+        }
+        if (!$inQuote && $c == ' ') {
+            if($lastChar != ' ') {
+                $curArg++;
+            }
+            $lastChar = $c;
+            continue;
+        }
+        $lastChar = $c;
+        if(!array_key_exists($curArg, $args)) {
+            $args[$curArg] = '';
+        }
+        $args[$curArg] .= $c;
+    }
+    return $args;
+}
+
